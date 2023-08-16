0941308f6fac486b557d64f4e5e605ef02065050
Ramón Orts <rdorts@hotmail.com>
2023-08-16 15:27:48 +0200

    testing logging

--- logger.py
+++ logger.py
@@ -1,11 +1,12 @@
 import time
 
 
+# def logger(text):
+#     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+#     print("{} | {}\n".format(timestamp, text))
 
 def logger(text):
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-    print("{} | {}\n".format(timestamp, text))
-
+    print(text)
 
 def info(text):
     text = f"[INFO] - {text}"
