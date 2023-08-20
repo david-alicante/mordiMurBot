07cdbcb387760f6300a6214498fac4642f4a60f9
Ramón Orts <rdorts@hotmail.com>
2023-08-20 10:27:45 +0200

    cambios logger y pruebas pole

--- logger.py
+++ logger.py
@@ -9,7 +9,7 @@ def logger(text):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log = "{} | {}\n".format(timestamp, text)
     print(log)
-    bot.send_message(admin_chat_id, admin_chat_id, log)
+    bot.send_message(admin_chat_id, log)
 
 
 def info(text):
